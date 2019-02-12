1f2e0c0f6c1c599e99422d89f637deac08a594b3
Temirlan <tima_tulebaev00@mail.ru>
2019-02-12 22:20:43 +0600

    Task

--- week_1/lab_1/Task 2/Program.cs
+++ week_1/lab_1/Task 2/Program.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 
-namespace Task_2
+namespace TasK
 {
+    class Student//Created class 
+    {
+        public string name;//method_name;
+        public string  id;//method_id
+        public int year;//method year
+
+        //public to use this methods in constructor
+
+        public Student()//constructor to transfer values to methods
+        {
+            name = Console.ReadLine();//to write value;
+            id  = Console.ReadLine();//to write value;
+            year  = int.Parse(Console.ReadLine());//to write value;
+
+            Print();//called a function
+
+        }
+
+        public void Print()//created function which will output values;
+        {
+            Console.WriteLine("Name : " + name);//to output value in nice_format
+            Console.WriteLine("ID : " + id);//to output value in nice_format
+            Console.WriteLine("Year : " + year);//to output value in nice_format
+
+        }
+
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            int line;// creating integer to know how many stars i want
-            line = Convert.ToInt32(Console.ReadLine());// input
-            for (int i = 1; i <= line; i++)//loop for 2d array
-            {
-                for (int j = 1; j <= line; j++)// also loop for 2d array
-                {
-                    if (i == j)// condition to output external stars
-                    {
-                        Console.Write("[*]");
-                    }
-                    if (i > j)// conditon for internal stars
-                        Console.Write("[*]");
-                }
-                Console.Write("\n");//to complete star stair
+            Student student = new Student();//created new_student
 
-            }
-            Console.ReadKey();// to console won't close
-            
+
+            Console.ReadKey();//to console won't close
         }
-                }
-            }
-        
-    
+    }
+}
 
---
+++ week_1/lab_1/Task 4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int line;// creating integer to know how many stars i want
+            line = Convert.ToInt32(Console.ReadLine());// input
+            for (int i = 1; i <= line; i++)//loop for 2d array
+            {
+                for (int j = 1; j <= line; j++)// also loop for 2d array
+                {
+                    if (i == j)// condition to output external stars
+                    {
+                        Console.Write("[*]");
+                    }
+                    if (i > j)// conditon for internal stars
+                        Console.Write("[*]");
+                }
+                Console.Write("\n");//to complete star stair
+
+            }
+            Console.ReadKey();// to console won't close
+            
+        }
+                }
+            }
+        
+    
+
